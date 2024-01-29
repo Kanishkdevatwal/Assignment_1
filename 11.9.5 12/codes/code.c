410718427aa3b5a98343d7ce0e2f17a60a69725e
Kanishkdevatwal <155838240+Kanishkdevatwal@users.noreply.github.com>
2024-01-29 15:31:00 +0530

    Add files via upload

--- 11.9.5 12/codes/code.c
+++ 11.9.5 12/codes/code.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int n, N;
+    int n;
     printf("\\This program will print the values of the terms of the sequence upto x(n)\\\n");
-    printf("Enter value of n: ");
-    scanf("%d", &N);
-
+ 
+    
     // Printing the series header
-    printf("... \nx(0) = 11\n");
+    printf("...\nx(-2)=0\n");
+    printf("x(-1)=0\n");
+    printf("x(0) = 11\n");
 
     // Calculate and print the terms of the series for n = 0 to 10 (you can change the range as needed)
-    for (n = 1; n <= N; ++n) {
+    for (n = 1; n <= 10; ++n) {
         printf("x(%d) = %d \n",n,11+2*n);
     }
     return 0;
--- 11.9.5 12/codes/main.py
+++ 11.9.5 12/codes/main.py
@@ -1,23 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define x(n)
+# Load data from the output.dat file
+file_path = 'output.dat'
+
+
 def x_n(n):
-    return np.where(n < 0, 0, 11 + 2 * n)
-
-# Get N from the user
-print("//This program will plot the terms of the sequence up to x(n)// \n")
-N = int(input("Enter the value of n: "))
-
-# Generate the sequence up to N using numpy
-n_values = np.arange(-2, N + 1)
+    return np.where(n < 0, 0,np.loadtxt(file_path))
+    
+# Generate n values from -2 to 10
+n_values = np.arange(-2, 11)
 x_values = x_n(n_values)
 
-# Plot the terms
+# Plot the graph
 plt.stem(n_values, x_values, markerfmt='o', linefmt='b-', basefmt='r-')
-plt.title('Terms of the Sequence x(n)')
+plt.title('Graph of x(n)')
 plt.xlabel('n')
 plt.ylabel('x(n)')
 plt.grid(True)
 plt.show()
-
