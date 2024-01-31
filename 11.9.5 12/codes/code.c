07d2c7621e890f76b3a0b599c8ff8445b1720b4a
Kanishkdevatwal <155838240+Kanishkdevatwal@users.noreply.github.com>
2024-01-31 09:37:12 +0530

    Add files via upload

--- 11.9.5 12/codes/code.c
+++ 11.9.5 12/codes/code.c
@@ -4,15 +4,19 @@ int main() {
     int n;
     printf("\\This program will print the values of the terms of the sequence upto x(n)\\\n");
  
-    
-    // Printing the series header
     printf("...\nx(-2)=0\n");
     printf("x(-1)=0\n");
     printf("x(0) = 11\n");
 
-    // Calculate and print the terms of the series for n = 0 to 10 (you can change the range as needed)
     for (n = 1; n <= 10; ++n) {
         printf("x(%d) = %d \n",n,11+2*n);
+    }
+
+     printf("...\ny(-2) = 0\n");
+    printf("y(-1) = 0\n");
+    printf("y(0) = 11\n");
+    for (n = 1; n <= 10; ++n) {
+         printf("y(%d) = %d \n",n,11*(n+1)+n*(n+1));
     }
     return 0;
 }
