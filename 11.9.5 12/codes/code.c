60ff61e778f7489acd046a9eb03d0771785c7584
Kanishkdevatwal <155838240+Kanishkdevatwal@users.noreply.github.com>
2024-02-01 21:15:30 +0530

    Add files via upload

--- 11.9.5 12/codes/code.c
+++ 11.9.5 12/codes/code.c
@@ -2,21 +2,36 @@
 
 int main() {
     int n;
-    printf("\\This program will print the values of the terms of the sequence upto x(n)\\\n");
- 
-    printf("...\nx(-2)=0\n");
-    printf("x(-1)=0\n");
-    printf("x(0) = 11\n");
+    FILE *outputFile;
 
-    for (n = 1; n <= 10; ++n) {
-        printf("x(%d) = %d \n",n,11+2*n);
+    // Open the file for writing
+    outputFile = fopen("output.dat", "w");
+
+    if (outputFile == NULL) {
+        printf("Error opening file for writing.\n");
+        return 1;
     }
 
-     printf("...\ny(-2) = 0\n");
-    printf("y(-1) = 0\n");
-    printf("y(0) = 11\n");
+    // Print the initial values to the console
+    printf("0\n");
+    printf("0\n");
+    printf("11\n");
+
+    // Print the initial values to the file
+    fprintf(outputFile, "0\n");
+    fprintf(outputFile, "0\n");
+    fprintf(outputFile, "11\n");
+
+    // Print the sequence values to the console and save them to the file
     for (n = 1; n <= 10; ++n) {
-         printf("y(%d) = %d \n",n,11*(n+1)+n*(n+1));
+        printf("%d \n", 11 * (n + 1) + n * (n + 1));
+        fprintf(outputFile, "%d \n", 11 * (n + 1) + n * (n + 1));
     }
+
+    // Close the file
+    fclose(outputFile);
+
+    printf("Output has been saved to 'output.dat'.\n");
+
     return 0;
 }
--- 11.9.5 12/codes/graphs.py
+++ 11.9.5 12/codes/graphs.py
@@ -2,28 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load data from the output.dat file
-file_path_x = 'output.dat'
-file_path_y = 'output2.dat'
-
-def x_n(n):
-    return np.where(n < 0, 0, np.loadtxt(file_path_x))
+file_path_y = 'output.dat'
 
 def y_n(n):
     return np.where(n < 0, 0, np.loadtxt(file_path_y))
 
 # Generate n values from -2 to 10
 n_values = np.arange(-2, 11)
-x_values = x_n(n_values)
 y_values = y_n(n_values)
-
-# Plot the graph for x(n)
-plt.stem(n_values, x_values, markerfmt='o', linefmt='b-', basefmt='r-')
-plt.title('Terms of the Sequence x(n)')
-plt.xlabel('n')
-plt.ylabel('x(n)')
-plt.grid(True)
-plt.savefig('fig.png')
-plt.close()
 
 # Plot the graph for y(n)
 plt.stem(n_values, y_values, markerfmt='o', linefmt='b-', basefmt='r-')
@@ -40,5 +26,6 @@ plt.xlabel('n')
 plt.ylabel('y(n)')
 plt.grid(True)
 plt.legend()
-plt.savefig('fig2.png')
+plt.savefig('fig.png')
 plt.close()
+
